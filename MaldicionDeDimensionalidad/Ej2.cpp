db2670b204f78dccb6af2db16a61996700cb0c3f
Fabian2727 <80995242+Fabian2727@users.noreply.github.com>
2022-09-12 23:03:50 -0500

    Update Ej2.cpp

--- MaldicionDeDimensionalidad/Ej2.cpp
+++ MaldicionDeDimensionalidad/Ej2.cpp
@@ -1,7 +1,7 @@
 #include <iostream>
 #include <stdlib.h>
 #include <math.h>
-#include <chrono>>
+#include <chrono>
 #include <vector>
 using namespace std;
 
@@ -32,5 +32,53 @@ int main() {
   }
   auto end4 = std::chrono::system_clock::now();
   std::chrono::duration<float,std::milli> duration4 = end4 - start4;
-  cout << "Matriz[100][1000]: " << duration4.count() << " sec. " << endl;
+  cout << "Ptro[100][1000]: " << duration4.count() << " sec. " << endl;
+  
+  auto start5 = std::chrono::system_clock::now();
+  long long mat5 [100][10000] = {0};
+  
+  for (long long i = 0; i< 100; i++)
+  {
+    for (long long j = 0; j<10000; j++)
+    {
+      mat5[i][j] = rand()%500;
+    }
+  }
+  
+  for (long long i = 1; i< 100; i++)
+  {
+    long long aux = 0;
+    for (long long j = 0; j<10000; j++)
+    {
+      aux += pow(mat5[i][j]-mat5[0][j],2);
+    }
+    aux = sqrt(aux);
+  }
+  auto end5 = std::chrono::system_clock::now();
+  std::chrono::duration<float,std::milli> duration5 = end5 - start5;
+  cout << "Ptro[10 000]: " << duration5.count() << " sec. " << endl;
+
+  auto start6 = std::chrono::system_clock::now();
+  long long mat6 [100][50000] = {0};
+  
+  for (long long i = 0; i< 100; i++)
+  {
+    for (long long j = 0; j<50000; j++)
+    {
+      mat6[i][j] = rand()%500;
+    }
+  }
+  
+  for (long long i = 1; i< 100; i++)
+  {
+    long long aux = 0;
+    for (long long j = 0; j<50000; j++)
+    {
+      aux += pow(mat6[i][j]-mat6[0][j],2);
+    }
+    aux = sqrt(aux);
+  }
+  auto end6 = std::chrono::system_clock::now();
+  std::chrono::duration<float,std::milli> duration6 = end6 - start6;
+  cout << "Ptro[50 000]: " << duration6.count() << " sec. " << endl;
 }
